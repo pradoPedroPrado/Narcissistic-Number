3be37868c57b6a10d443cff086d9e922201a7e72
Pedro Prado Win <126698557+pradoPedroPrado@users.noreply.github.com>
2023-04-11 01:58:58 -0300

    Add project files.

---
+++ Kata.cs
@@ -0,0 +1,19 @@
+﻿namespace Narcissistic_Number
+{
+    internal class Kata
+    {
+        public static bool Narcissistic(int value)
+        {
+            String valueStr = value.ToString();
+            Double[] digits = new double[valueStr.Length];
+            Double sum = 0;
+            for (int i = 0; i < valueStr.Length; i++)
+            {
+                digits[i] = Math.Pow(double.Parse(valueStr[i].ToString()), valueStr.Length) ;
+                Console.WriteLine(digits[i]);
+                sum += digits[i];
+            }
+            return (value == sum);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Narcissistic_Number;
+
+Console.WriteLine(Kata.Narcissistic(153));
+Console.WriteLine(Kata.Narcissistic(154));
+Console.WriteLine(Kata.Narcissistic(378));
+//Console.WriteLine(Math.Pow(10,3));
